bee8d4d8df104c423ea8094a03c646dbfe551513
XanderEcran <xander.cooperation@gmail.com>
2025-04-21 11:24:03 +0700

    Making POST Comman

--- src/app/matkul/page.jsx
+++ src/app/matkul/page.jsx
@@ -4,6 +4,10 @@ import {useEffect, useState} from 'react';
 
 export default function MatkulPage(){
     const [matkuls, setMatkuls] = useState ([]);
+    const [kode, setKode] = useState('');
+    const [nama, setNama] = useState('');
+    const [msg, setMsg] = useState('');
+    const [formVisible, setFormVisible] = useState(false);
 
     const fetchMatkuls = async () => {
         const res = await fetch('/api/matkul');
@@ -14,8 +18,57 @@ export default function MatkulPage(){
         fetchMatkuls();
     }, []);
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const res = await fetch ('/api/matkul', {
+            method : 'POST',
+            header : {'Content Type' : 'application/json'},
+            body : JSON.stringify({kode, nama}),
+        });
+
+        if(res.ok){
+            setMsg('Berhasil disimpan!');
+            setKode('');
+            setNama('');
+            setFormVisible(false);
+            fetchMatkuls();
+        }else {
+            setMsg('Gagal menyimpan data');
+        }
+    };
+
     return (
         <div>
+            <button
+                onClick={() => setFormVisible(!formVisible)}>
+                {formVisible ? 'Tutup Form' : 'Tambah Matkul'}
+            </button>
+            {formVisible && (
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <input
+                            type='text'
+                            value={kode}
+                            onChange={(e) => setKode(e.target.value)}
+                            placeholder='Masukkan Kode'
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type='text'
+                            value={nama}
+                            onChange={(e) => setNama(e.target.value)}
+                            placeholder='Masukkan Nama Matkul'
+                            required
+                        />
+                    </div>
+                    <button type='submit'>
+                        Simpan
+                    </button>
+                    <p>{msg}</p>
+                </form>
+            )}
             <table border="1">
                 <thead>
                     <tr>
