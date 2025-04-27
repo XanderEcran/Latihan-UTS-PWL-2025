62f14a8294577f7ab1291736b3549e0b4ed74220
XanderEcran <xander.cooperation@gmail.com>
2025-04-27 20:39:49 +0700

    Create for Prodi

--- src/app/api/prodi/route.js
+++ src/app/api/prodi/route.js
@@ -6,3 +6,17 @@ export async function GET(){
     });
     return new Response(JSON.stringify(data), {status:200});
 }
+
+export async function POST(request) {
+    const {kode, nama, kepala} = await request.json();
+
+    if (!kode || !nama || !kepala) {
+        return new Response(JSON.stringify({error: 'Semua field wahib diisi'}), {
+            status: 400, 
+        });
+    }
+    const prodi = await prisma.prodi.create({
+        data: {kode, nama, kepala},
+    });
+    return new Response(JSON.stringify(prodi), {status: 201});
+}
--- src/app/prodi/page.jsx
+++ src/app/prodi/page.jsx
@@ -4,6 +4,11 @@ import { useEffect, useState } from "react";
 
 export default function ProdiPage() {
     const [prodis, setProdis] = useState([]);
+    const [kode, setKode] = useState('');
+    const [nama, setNama] = useState('');
+    const [kepala, setKepala] = useState('');
+    const [msg, setMsg] = useState('');
+    const [formVisible, setFormVisible] = useState(false);
 
     const fetchProdis = async () => {
         const res = await fetch('/api/prodi');
@@ -15,8 +20,67 @@ export default function ProdiPage() {
         fetchProdis();
     }, []);
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const res = await fetch('/api/prodi', {
+            method : 'POST',
+            headers : {'Content-Type' : 'application/json'},
+            body : JSON.stringify({kode, nama, kepala}),
+        });
+        if (res.ok) {
+            setMsg('Berhasil disimpan!');
+            setKode('');
+            setNama('');
+            setKepala('');
+            setFormVisible(false);
+            fetchProdis();
+        }else {
+            setMsg('Gagal menyimpan data');
+        }
+    }
+
     return (
         <div>
+            <button
+                onClick={() => setFormVisible(!formVisible)}>
+                {formVisible ? 'Tutup Form' : 'Tambah Prodi'}
+            </button>
+            {formVisible && (
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <input
+                            type="text"
+                            value={kode}
+                            onChange={(e) => setKode(e.target.value)}
+                            placeholder="Masukkan Kode"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            value={nama}
+                            onChange={(e) => setNama(e.target.value)}
+                            placeholder="Masukkan Nama"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            value={kepala}
+                            onChange={(e) => setKepala(e.target.value)}
+                            placeholder="Masukkan Nama Dosen"
+                            required
+                        />
+                    </div>
+                    <button type="submit">
+                        Simpan
+                    </button>
+                    <p>{msg}</p>
+                </form>
+            )}
+            <br></br>
             <table border="1">
                 <thead>
                     <tr>
